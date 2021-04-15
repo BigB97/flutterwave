9707da1236f93f8cdf810eb9464251633042a34c
Yusuf Bolaji <61923150+BigB97@users.noreply.github.com>
2021-04-15 21:30:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -19,5 +19,5 @@ app.use(cookieParser());
 app.use('/', indexRouter);
 app.use('/validate-rule', usersRouter);
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`:::> Server listening on port ${process.env.PORT} || 3000`);
+  console.log(`:::::> Server listening on port ${process.env.PORT} || 3000`);
 });
